02b2f5e95126c1f11b5314911db6988b7c35e648
Anthony Pagano <apagano51@gmail.com>
2019-03-17 15:41:57 -0400

    master: added data model schema

---
+++ models.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const beersSchema = mongoose.Schema({
+  beerName: {type: String, required: true },
+  style: {type: String, required: true },
+  abv: {type: String, required: true },
+  brewery: {type: String, required: true },
+  cityState: {type: String, required: true },
+  notes: {type: String, required: true },
+  dateAdded: {type: Date, default: Date.now }
+});
+
+beersSchema.methods.serialize = function() {
+  return {
+    id: this._id,
+    beerName: this.beerName,
+    style: this.style,
+    abv: this.abv,
+    brewery: this.brewery,
+    rating: this.rating,
+    cityState: this.cityState,
+    notes: this.notes,
+    dateAdded: this.dateAdded
+  };
+};
+
+const Beers = mongoose.model('Beers', beersSchema);
+
+module.exports = {Beer};
