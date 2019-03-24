163664e57bb4819876db9d2d36e12b6fd6f183ae
Anthony Pagano <apagano51@gmail.com>
2019-03-24 16:19:32 -0400

    endpoints: added tests for endpoints

--- test/test-server.js
+++ test/test-server.js
@@ -2,7 +2,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
-const {Beers} = require('../models');
+const {Beer} = require('../models');
 const {app, runServer, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
 const should = chai.should();
@@ -20,7 +20,7 @@ function seedEntryData() {
     { beerName: "Bourbon County Stout", style: "Stout", abv: "15", brewery: "Goose Island Brewing Company", rating: "5", cityState: "Chicago, IL", notes: "a black friday tradition"},
   ];
 
-  return Beers.insertMany(seedData);
+  return Beer.insertMany(seedData);
 }
 
 function tearDownDb() {
@@ -28,17 +28,26 @@ function tearDownDb() {
   return mongoose.connection.dropDatabase();
 }
 
+describe("The Hop Index app", function() {
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function () {
+    return seedEntryData();
+  });
+
+  afterEach(function () {
+    return tearDownDb();
+  });
+
+  after(function() {
+    return closeServer();
+  });
+
 describe('Endpoint Tests', function(){
   console.log('TEST DB IS ' + TEST_DATABASE_URL)
-  it('should 200 on GET requests', function() {
-    return chai.request(app)
-      .get('/fooooo')
-      .then(function(res) {
-        res.should.have.status(200);
-        res.should.be.json;
-      });
-  });
-  
+
   it('Should return index.html', function () {
     return chai.request(app)
       .get('/')
@@ -53,14 +62,84 @@ describe('Endpoint Tests', function(){
       .get('/beer')
       .then(_res => {
         res = _res;
-        console.log('it got into the return all beers test')
         res.should.have.status(200);
         res.body.should.have.lengthOf.at.least(1);
   
-        return Beers.count();
+        return Beer.countDocuments();
       })
       .then(count => {
         res.body.should.have.lengthOf(count);
       });
   });    
+
+  it('should return 5 most recent beers tried', function() {
+    let res;
+    return chai.request(app)
+      .get('/recent')
+      .then(_res => {
+        res = _res;
+        res.should.have.status(200);
+        res.body.should.have.lengthOf.at.least(5);
+      })
+  });
+
+  it('should add a new beer', function() {
+
+    const newBeer = {
+      beerName: "Whammy",
+      style: "IIPA",
+      abv: "8.0",
+      brewery: "Singlecut Beersmiths",
+      rating: "4",
+      cityState: "Astoria, NY",        
+      notes: "named after Press Your Luck"
+    };
+
+    return chai.request(app)
+    .post('/beer')
+    .send(newBeer)
+    .then(function (res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('id', 'beerName', 'style', 'abv', 'brewery', 'rating', 'cityState', 'notes');
+      // cause Mongo should have created id on insertion
+      res.body.id.should.not.be.null;
+      res.body.beerName.should.equal(newBeer.beerName);
+      res.body.style.should.equal(newBeer.style);
+      res.body.abv.should.equal(newBeer.abv);
+      res.body.brewery.should.equal(newBeer.brewery);        
+      res.body.rating.should.equal(newBeer.rating);        
+      res.body.cityState.should.equal(newBeer.cityState);                
+      res.body.notes.should.equal(newBeer.notes);   
+      return Beer.findById(res.body.id);
+    })
+    .then(function (post) {
+      post.beerName.should.equal(newBeer.beerName);
+      post.style.should.equal(newBeer.style);
+      post.abv.should.equal(newBeer.abv);
+      post.brewery.should.equal(newBeer.brewery);
+      post.rating.should.equal(newBeer.rating);
+      post.cityState.should.equal(newBeer.cityState);        
+      post.notes.should.equal(newBeer.notes);
+    });
+  });  
+
+  it('delete a beer by id', function() {
+
+    let beer;
+
+    return Beer
+        .findOneAndDelete()
+        .then(_beer => {
+        beer = _beer;
+        return chai.request(app).delete(`/beer/${beer.id}`);
+        })
+        .then(res => {
+        res.should.have.status(204);
+        return Beer.findById(beer.id);
+        })
+    });
+
+});
 });
