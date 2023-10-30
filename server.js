78868965bd43c7cc5bb80e44b6fc6f908a4c178c
anthonypagano <apagano51@gmail.com>
2023-10-29 21:01:59 -0400

    back to orig

--- server.js
+++ server.js
@@ -18,24 +18,6 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
-
-app.use((req, res, next) => {
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
-
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
-
 app.use(
   cors({
       origin: CLIENT_ORIGIN
