5a3f17c3cc7735b9a16529944a7ce44818f3b8f7
anthonypagano <apagano51@gmail.com>
2023-10-31 10:31:26 -0400

    rollback to last known working state

--- config.js
+++ config.js
@@ -1,11 +1,8 @@
 'use strict';
-exports.API_ORIGIN = 'https://hopindex-api-bc2ac5fac72f.herokuapp.com';
-exports.CLIENT_ORIGIN = 'https://hopindex-client-c04b1bc747fd.herokuapp.com';
-exports.DATABASE_URL = 'mongodb+srv://apagano76:76apagano@hopindex.jbte0v1.mongodb.net/?retryWrites=true&w=majority';
-exports.PORT = process.env.PORT || 8080;
-//exports.API_ORIGIN = 'https://dry-depths-20265.herokuapp.com';
-//exports.CLIENT_ORIGIN = 'https://shrouded-sierra-74421.herokuapp.com';
-//exports.DATABASE_URL = 'mongodb+srv://apagano76:76apagano@react-capstone.euuin.mongodb.net/react-capstone?retryWrites=true&w=majority';
-//exports.TEST_DATABASE_URL = 'mongodb+srv://apagano76:76apagano@react-capstone-test.ip7re.mongodb.net/react-capstone-test?retryWrites=true&w=majority';
+exports.API_ORIGIN = 'https://dry-depths-20265.herokuapp.com';
+exports.CLIENT_ORIGIN = 'https://shrouded-sierra-74421.herokuapp.com';
+exports.DATABASE_URL = 'mongodb+srv://apagano76:76apagano@react-capstone.euuin.mongodb.net/react-capstone?retryWrites=true&w=majority';
+exports.TEST_DATABASE_URL = 'mongodb+srv://apagano76:76apagano@react-capstone-test.ip7re.mongodb.net/react-capstone-test?retryWrites=true&w=majority';
 //exports.DATABASE_URL = 'mongodb://apagano76:76apagano@ds121282.mlab.com:21282/react-capstone';
 //exports.TEST_DATABASE_URL = 'mongodb://apagano76:76apagano@ds229878.mlab.com:29878/react-capstone-test';
+exports.PORT = process.env.PORT || 8080;
